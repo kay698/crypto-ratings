e4ed52a6b14f3fe4d621598fcf15c30be5a65b83
kay698 <ogbukakelechi20@gmail.com>
2022-02-13 19:57:45 +0100

    pages moved

--- src/pages/LandingPage/index.jsx
+++ src/pages/LandingPage/index.jsx
@@ -1,10 +1,30 @@
 import React from "react";
 import LandingLayout from "../../components/Layout";
+import { LandingPageWrapper } from "./styles";
+import { FlexibleDiv } from "../../components/Box/styles";
+import { Typography } from "antd";
+import HeroCards from "../../assets/pngs/hero-cards.png";
 
 const LandingPage = () => {
   return (
-    <LandingLayout>
-      <div>Hey</div>;
+    <LandingLayout noPadding>
+      <LandingPageWrapper>
+        <FlexibleDiv>
+          <FlexibleDiv flexDir="column" className="hero">
+            <p>The Best Plug to –</p>
+            <Typography.Title>
+              Trade your <span>Giftcards</span> & <span>Bitcoin</span> for Cash
+              Instantly.
+            </Typography.Title>
+            <span>
+              Trading your giftcards & bitcoin for cash has never been easy.
+              Trade securely with no worries or delay. Receive alert of your
+              trades in less than 2 mins.
+            </span>
+            <img src={HeroCards} alt="" />
+          </FlexibleDiv>
+        </FlexibleDiv>
+      </LandingPageWrapper>
     </LandingLayout>
   );
 };
